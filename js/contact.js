66327df3e77c86f0ed70f2cf4d330b989436ae27
RaulSaenzJR <newstarblues@gmail.com>
2022-08-08 22:10:29 -0500

    updated to meet all guidelines

--- js/contact.js
+++ js/contact.js
@@ -2,11 +2,9 @@ const steps = Array.from(document.querySelectorAll('form .step'));
 const nextBtn = document.querySelectorAll('form .next-btn');
 const prevBtn = document.querySelectorAll('form .previous-btn');
 const form = document.querySelector('form');
-
-
 nextBtn.forEach(button=>{
     button.addEventListener('click', (e) =>{
-     changeStep('next');   
+        changeStep('next');   
     })
 })
 prevBtn.forEach(button=> {
@@ -14,21 +12,21 @@ prevBtn.forEach(button=> {
         changeStep('prev')
     }) 
 })
-    form.addEventListener('submit', (e)=>{
-        e.preventDefault();
-        const inputs = [];
-        form.querySelectorAll('input').forEach(input=>{
-            const {name, value} = input;
-            inputs.push({name, value})
-        })
-        console.log(inputs)
-         form.reset();
-         let index = 0;
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const inputs = [];
+    form.querySelectorAll('input').forEach(input=>{
+        const {name, value} = input;
+        inputs.push({name, value})
+    })
+    console.log(inputs)
+    form.reset();
+    let index = 0;
     const active = document.querySelector('form .step.active');
     index = steps.indexOf(active);
     steps[index].classList.remove('active'); 
     steps[0].classList.add('active');
-    })
+})
 function changeStep(btn){
     let index = 0;
     const active = document.querySelector('form .step.active');
@@ -36,7 +34,8 @@ function changeStep(btn){
     steps[index].classList.remove('active'); 
     if(btn === 'next'){
         index ++;
-    }else if(btn === 'prev'){
+    }
+    else if(btn === 'prev'){
         index --;
     }
     steps[index].classList.add('active')
